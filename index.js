0752a5d13320987ed9feff5a8a205248e36a09da
ridwan414-hub <asifmahmoud414@gmail.com>
2024-03-09 14:00:36 +0600

    again hello world

--- index.js
+++ index.js
@@ -35,9 +35,9 @@ let persons =
         }
     ]
 
-// app.get('/', (request, response) => {
-//     response.send('<h1>Hello World!</h1>')
-// })
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+})
 app.get('/info', (request, response) => {
     const date = new Date();
     response.send(`<p>Phonebook has info for ${persons.length} people </p><br><p> ${date} </p>`);
