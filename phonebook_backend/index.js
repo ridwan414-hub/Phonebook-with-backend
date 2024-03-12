3d7565638526f68a7acb5be6dc862204f4551963
ridwan414-hub <asifmahmoud414@gmail.com>
2024-03-12 22:17:29 +0600

    delet for db added

--- phonebook_backend/index.js
+++ phonebook_backend/index.js
@@ -84,10 +84,12 @@ app.put(`/api/persons/:id`, (request, response) => {
 
 
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
 
